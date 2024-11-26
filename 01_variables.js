8b0813b7fba9a00d6d13f39122923ee0c5949ce9
bhupinder <bhupindernh784@gmail.com>
2024-11-26 11:31:54 +0530

    learn var, let & cont

---
+++ 01_variables.js
@@ -0,0 +1,18 @@
+const accoundId = 1234;
+
+//accoundId = 1010;  not allowed
+//console.log(accoundId);
+
+let email = "dinga@gmail.com";
+var password = "112233";
+city = "Banglore";
+
+email = "zizi@gnail.com"
+password = "101010101"
+city = "Mumbai"
+let state;
+
+console.log(email);
+console.log(password);
+console.log(city);
+console.log(state); //undefined
