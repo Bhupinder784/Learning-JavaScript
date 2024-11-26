a3e674fa67ac83486e653c4d647b067f7a85de8c
bhupinder <bhupindernh784@gmail.com>
2024-11-26 11:54:29 +0530

    data types

---
+++ 02_DataType.js
@@ -0,0 +1,16 @@
+"use  strick"; //from now js will treat only as new version
+
+let name = "Dinga" // String
+let age = 23       //int or number
+let isLoggedIn =  false //boolean
+let city;           //null
+
+//number
+//bigInt
+//string
+//boolean
+//null  -> empty
+//undefine 
+//symbol    -> unique
+
+//Object
